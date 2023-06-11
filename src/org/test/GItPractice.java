95770b4e5bc12569f0415ebe05839715a037a963
ItsUnicMadhan <111955362+ItsUnicMadhan@users.noreply.github.com>
2023-06-11 19:45:41 +0530

    Update GItPractice.java

--- src/org/test/GItPractice.java
+++ src/org/test/GItPractice.java
@@ -13,16 +13,4 @@ public class GItPractice {
 	public void method3() {
 		System.out.println("Method Three");
 	}
-
-	public void method4() {
-		System.out.println("Method four");
-	}
-
-	public static void main(String[] args) {
-		GItPractice gItPractice = new GItPractice();
-		gItPractice.method5();
-		gItPractice.method2();
-		gItPractice.method3();
-		gItPractice.method4();
-	}
 }
